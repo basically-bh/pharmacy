253703829ab2927121c92bd525c05605398b31d8
Talal Aljishi <talal@aljishi.com>
2026-03-12 11:33:35 +0300

    fix(pharmacy): restore app order orchestration flow

--- pharmacy/pharmacy/doctype/app_order/app_order.js
+++ pharmacy/pharmacy/doctype/app_order/app_order.js
@@ -7,7 +7,52 @@ function refreshAppOrderPricing(frm) {
 	}
 
 	frm.__app_order_syncing = true;
-	return frm.call("refresh_pricing_for_form")
+	return frappe.call({
+		method: "pharmacy.pharmacy.doctype.app_order.app_order.refresh_app_order_pricing",
+		args: { doc: frm.doc }
+	})
+		.then((r) => {
+			const data = r.message || {};
+			const itemRowsByName = new Map((frm.doc.items || []).map((row) => [row.name, row]));
+			const parentUpdates = {};
+
+			["customer", "contact_mobile", "company", "currency", "price_list", "subtotal", "tax_amount", "grand_total"].forEach((fieldname) => {
+				if (data[fieldname] !== undefined && frm.doc[fieldname] !== data[fieldname]) {
+					parentUpdates[fieldname] = data[fieldname];
+				}
+			});
+
+			if (!frm.doc.delivery_address && data.delivery_address) {
+				parentUpdates.delivery_address = data.delivery_address;
+			}
+
+			const parentUpdatePromise = Object.keys(parentUpdates).length
+				? frm.set_value(parentUpdates)
+				: Promise.resolve();
+
+			return parentUpdatePromise.then(() => {
+				(data.items || []).forEach((itemData) => {
+					const row = itemRowsByName.get(itemData.name);
+					if (!row) {
+						return;
+					}
+
+					[
+						"item_name",
+						"uom",
+						"rate",
+						"amount",
+						"vat_rate",
+						"vat_amount",
+						"total_amount",
+					].forEach((fieldname) => {
+						if (row[fieldname] !== itemData[fieldname]) {
+							frappe.model.set_value(row.doctype, row.name, fieldname, itemData[fieldname]);
+						}
+					});
+				});
+			});
+		})
 		.then(() => {
 			frm.refresh_fields(["company", "currency", "price_list", "contact_mobile", "delivery_address", "items", "subtotal", "tax_amount", "grand_total"]);
 		})
